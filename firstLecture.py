851985d45ae4433312e1b9c5a531cf88ce3db7f4
Ayeza <ayezabashir442@gmail.com>
2024-02-26 22:43:47 +0500

    data types

---
+++ firstLecture.py
@@ -0,0 +1,17 @@
+# You can get the data type of any object by using the type() function:
+a = 5  # int
+b = "hello world"  # string
+c = 20.5  # float
+d = 1j  # complex
+e = ["apple", "banana", "cherry"]  # list
+f = ("apple", "banana", "cherry")  # tuple
+g = range(6)
+h = {"name": "Ayeza", "degree": "MPhill"}  # dict (dictionary)
+i = frozenset({"apple", "banana", "cherry"})  # frozenset
+j = True  # boolean
+k = b"Hello"  # bytes
+l = bytearray(5)  # bytearray
+m = memoryview(bytes(5))  # memoryview
+n = None  # Nonetype
+
+print(type(n))
