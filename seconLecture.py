1089fcf058c90250d07c2f0ad89cbe9a969fa87e
Ayeza <ayezabashir442@gmail.com>
2024-03-04 18:09:13 +0500

    while loop

--- seconLecture.py
+++ seconLecture.py
@@ -1,15 +1,26 @@
 # if condition syntax
 a = 14
 b = 12
-if(a > b):
+if a > b:
     print('a is greater than b')
+
+# ternary operator
+print("a") if a > b else print("b")
 
 #  id else if and else syntax
 c = 15
 d = 15
-if(c > d):
+if c > d:
     print('c is greater than d')
-elif(d > c):
+elif d > c:
     print('d is greater than c')
 else:
     print('c is equal to d')
+
+#  loops
+#  python has two loops "while" and "for"
+#  while loop
+i = 1
+while i < 6:
+    print(i)
+    i += 1
