f82dc08398db559a036fa78131e53a439681ad8c
Ayeza <ayezabashir442@gmail.com>
2024-03-04 17:23:10 +0500

    second lecture

---
+++ seconLecture.py
@@ -0,0 +1,15 @@
+# if condition syntax
+a = 14
+b = 12
+if(a > b):
+    print('a is greater than b')
+
+#  id else if and else syntax
+c = 15
+d = 15
+if(c > d):
+    print('c is greater than d')
+elif(d > c):
+    print('d is greater than c')
+else:
+    print('c is equal to d')
