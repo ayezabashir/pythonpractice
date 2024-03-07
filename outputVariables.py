f0bc8c73506911f2787520bc7c8cb6643db53499
Ayeza <ayezabashir442@gmail.com>
2024-03-07 22:48:37 +0500

    output variables

---
+++ outputVariables.py
@@ -0,0 +1,17 @@
+#  python use print() for outputing the value
+
+# we can output multiple variables by using comma
+x = 'hi'
+y = 'biatch'
+print(x,y)
+
+# we can also use + to output variables
+a = 'hi'
+b = 'ayeza'
+print(a, b)
+
+# with numbers it would be treated as an operation
+c = 20
+d = 21
+print(c+d)
+print(c,d)
