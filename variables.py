3e259b1c6846dd1bf473c7c92beff6a99b388709
Ayeza <ayezabashir442@gmail.com>
2024-03-07 21:30:48 +0500

    variables

--- variables.py
+++ variables.py
@@ -2,6 +2,40 @@
 #  a value to it
 x = 5
 y = "Hello, World!"
-
 print(x)
 print(y)
+
+# If you want to specify the data type of a variable,
+# this can be done with CASTING.
+a = str(3)   # 3 will be treated as a string
+b = int(3)   # 3 will be treated as an integer
+c = float(3)  # 3 will be treated as a floating point i.e 3.0
+
+# we can get the type of variable using type()
+print(type(a))
+print(type(b))
+print(type(c))
+
+# String variables can be declared either by
+# using single or double quotes:
+name = 'ayeza'   # this way
+name1 = "ayeza"  # or this way
+
+# Variable names are case-sensitive.
+email = "ayezabashir46@gmail.com"
+Email = "aqsabashir46@gmail.com"
+print(email)
+print(Email)
+
+# Python allows you to assign values to multiple
+# variables in one line
+x, y, z = 2, 'hello', 3.4
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+p = q = r = "Oranges"
+print(p)
+print(r)
+print(q)
