559267eeb7756e48524e5e1f03908dcd44758604
Ayeza <ayezabashir442@gmail.com>
2024-03-09 00:00:54 +0500

    modify stringgss

---
+++ modifyStrings.py
@@ -0,0 +1,8 @@
+# built-in methods that we can   use on strings
+
+a = "AyEza"
+print(a.upper())  # convert to uppercase
+print(a.lower())  # convert to lowercase
+
+b = " Hi, blah "
+print(b.strip())  # remove whitespaces from beginning and end
