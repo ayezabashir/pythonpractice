c37b354c0b28df98e0d8ad21b83aaddf66cff17a
Ayeza <ayezabashir442@gmail.com>
2024-03-11 17:18:20 +0500

    operators

---
+++ operators/operators.py
@@ -0,0 +1,33 @@
+# operators are used to perform operations on values
+# python divides the operators in 7 divisions
+
+# 1- ARITHMETIC OPERATORS
+print(7 + 7)
+print(7 - 7)
+print(7 / 7)
+print(7 // 7)  # floor division
+print(7 % 7)
+print(7 * 7)
+print(7 ** 7)  # exponentiation
+
+# 2- ASSIGNMENT OPERATORS
+a = 5
+print(a)
+a +=5
+print(a)
+a -=5
+print(a)
+a=15
+a *=5
+print(a)
+a=15
+a /=5
+print(a)
+
+a=15
+a %=5
+print(a)
+a=15
+a //=5
+print(a)
+
