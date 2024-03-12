a0d3bf9a98a166d4a3f65d31674ad69eb99d21e7
Ayeza <ayezabashir442@gmail.com>
2024-03-12 20:20:25 +0500

    membership operator

--- operators/operators.py
+++ operators/operators.py
@@ -54,3 +54,9 @@ print(not(5<7))
 # IDENTITY OPERATOR
 print(5 is 5)
 print(6 is not 5)
+
+# MEMBERSHIP OPERATOR
+x = range(0,9)
+print(4 in x)
+fruits = ['apple', 'banana']
+print('orange' not in x)
