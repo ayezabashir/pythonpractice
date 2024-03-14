6b79df9ef84438e168d59b0031bf015674df9ada
Ayeza <ayezabashir442@gmail.com>
2024-03-14 22:47:52 +0500

    lists

---
+++ lists.py
@@ -0,0 +1,16 @@
+# Lists are one of 4 built-in data types
+# in Python used to store collections of data,
+# the other 3 are Tuple, Set, and Dictionary,
+
+# lists are:
+# 1-ordered
+# 2-changeable
+# 3-allow duplicate values.
+fruits = ["apples", "oranges", "bananas", "oranges"]
+list1 = [True, False, True]
+list2= [1,3,5,7]
+print(fruits)
+print(len(fruits))
+print(list2)
+print(list1)
+print(type(list2))
